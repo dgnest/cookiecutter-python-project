4b33f6eb81c8a8192079b2fe75b89fd478410564
@slovacus <slovacus@gmail.com>
2016-11-18 08:37:00 -0500

    [#2] Add: settings project

--- {{cookiecutter.project_name}}/script/clean.sh
+++ {{cookiecutter.project_name}}/script/clean.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# shellcheck disable=SC1091
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
 {
--- {{cookiecutter.project_name}}/script/sync.sh
+++ {{cookiecutter.project_name}}/script/sync.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # -*- coding: utf-8 -*-
 
 # shellcheck source=script/bootstrap.sh
-# shellcheck disable=SC1091
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-grip --pass "${GITHUB_API_TOKEN}" ${GRIP_PORT}
